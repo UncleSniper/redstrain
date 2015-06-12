1e6483f24e99550319c2cc8fb141ae787c3ef3d9
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-06-12 03:25:14 +0200

    added missing blank line to StopExecution.cpp

--- modules/cmdline/src/StopExecution.cpp
+++ modules/cmdline/src/StopExecution.cpp
@@ -4,6 +4,7 @@ namespace redengine {
 namespace cmdline {
 
 	StopExecution::StopExecution(int status) : status(status) {}
+
 	StopExecution::StopExecution(const StopExecution& stop) : status(stop.status) {}
 
 }}
