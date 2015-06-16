601e14bff0c6bc3f3270eb7ba4eda14b39835b5c
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-06-16 04:20:10 +0200

    added missing blank line to platform/src/Stat.cpp

--- modules/platform/src/Stat.cpp
+++ modules/platform/src/Stat.cpp
@@ -15,6 +15,7 @@ namespace platform {
 	Stat::Stat() : type(REGULAR_FILE), owner(NO_USER), group(NO_GROUP), device(NO_DEVICE), permissions(0),
 			size(static_cast<size_t>(0u)), atime(static_cast<time_t>(0u)),
 			mtime(static_cast<time_t>(0u)), ctime(static_cast<time_t>(0u)) {}
+
 	Stat::Stat(const Stat& stat) : type(stat.type), owner(stat.owner), group(stat.group), device(stat.device),
 			permissions(stat.permissions), size(stat.size), atime(stat.atime), mtime(stat.mtime),
 			ctime(stat.ctime) {}
