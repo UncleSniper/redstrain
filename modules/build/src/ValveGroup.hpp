659227d9688c5da84b7017c0a008c29948eb4db0
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-07-19 22:50:24 +0200

    added ValveGroup

---
+++ modules/build/src/ValveGroup.hpp
@@ -0,0 +1,51 @@
+#ifndef REDSTRAIN_MOD_BUILD_VALVEGROUP_HPP
+#define REDSTRAIN_MOD_BUILD_VALVEGROUP_HPP
+
+#include <set>
+#include <redstrain/util/ReferenceCounted.hpp>
+
+#include "api.hpp"
+
+namespace redengine {
+namespace build {
+
+	class Valve;
+	class StaticValve;
+	class BuildContext;
+
+	class REDSTRAIN_BUILD_API ValveGroup : public util::ReferenceCounted {
+
+	  private:
+		typedef std::set<Valve*> Valves;
+		typedef std::set<StaticValve*> StaticValves;
+
+	  public:
+		typedef Valves::const_iterator ValveIterator;
+		typedef StaticValves::const_iterator StaticValveIterator;
+
+	  private:
+		Valves valves;
+		StaticValves defaults;
+
+	  public:
+		ValveGroup();
+		ValveGroup(const ValveGroup&);
+		~ValveGroup();
+
+		bool addMember(Valve*);
+		bool removeMember(Valve*);
+		void clearMembers();
+		void getMembers(ValveIterator&, ValveIterator&);
+
+		bool addDefault(StaticValve*);
+		bool removeDefault(StaticValve*);
+		void clearDefaults();
+		void getDefaults(StaticValveIterator&, StaticValveIterator&);
+
+		bool forceDefaults(BuildContext&);
+
+	};
+
+}}
+
+#endif /* REDSTRAIN_MOD_BUILD_VALVEGROUP_HPP */
