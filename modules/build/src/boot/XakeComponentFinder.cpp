361b065899d11da0806b561f01d9c5145fb7e022
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-07-20 21:57:16 +0200

    added XakeComponentFinder

---
+++ modules/build/src/boot/XakeComponentFinder.cpp
@@ -0,0 +1,49 @@
+#include "XakeProject.hpp"
+#include "XakeComponentFinder.hpp"
+
+using std::string;
+
+namespace redengine {
+namespace build {
+namespace boot {
+
+	const char *const XakeComponentFinder::DEFAULT_LIBRARIES_DIRECTORY = "modules";
+	const char *const XakeComponentFinder::DEFAULT_BINARIES_DIRECTORY = "tools";
+
+	XakeComponentFinder::XakeComponentFinder(const XakeProject& project) : project(project) {}
+
+	XakeComponentFinder::XakeComponentFinder(const XakeComponentFinder& finder)
+			: DirectoryComponentFinder(finder), project(finder.project),
+			defaultLibrariesDirectory(finder.defaultLibrariesDirectory),
+			defaultBinariesDirectory(finder.defaultBinariesDirectory) {}
+
+	void XakeComponentFinder::setDefaultLibrariesDirectory(const string& directory) {
+		defaultLibrariesDirectory = directory;
+	}
+
+	void XakeComponentFinder::setDefaultBinariesDirectory(const string& directory) {
+		defaultBinariesDirectory = directory;
+	}
+
+	void XakeComponentFinder::findComponents(const Project& buildingProject, ProjectBuilder& builder) {
+		const Resources& configuration = project.getProjectConfiguration();
+		string directory(configuration.getProperty(Resources::RES_LIBRARIES_DIRECTORY));
+		if(directory.empty()) {
+			if(defaultLibrariesDirectory.empty())
+				directory = XakeComponentFinder::DEFAULT_LIBRARIES_DIRECTORY;
+			else
+				directory = defaultLibrariesDirectory;
+		}
+		addComponentsDirectory(directory, Component::LIBRARY);
+		directory = configuration.getProperty(Resources::RES_BINARIES_DIRECTORY);
+		if(directory.empty()) {
+			if(defaultBinariesDirectory.empty())
+				directory = XakeComponentFinder::DEFAULT_BINARIES_DIRECTORY;
+			else
+				directory = defaultBinariesDirectory;
+		}
+		addComponentsDirectory(directory, Component::EXECUTABLE);
+		DirectoryComponentFinder::findComponents(buildingProject, builder);
+	}
+
+}}}
