dfb142718e59f211da23b80e8bdb1372bc611ef7
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-07-28 12:13:51 +0200

    fixed copy-ctor of NoSuchValveError

--- modules/build/src/NoSuchValveError.cpp
+++ modules/build/src/NoSuchValveError.cpp
@@ -7,7 +7,7 @@ namespace build {
 
 	NoSuchValveError::NoSuchValveError(const string& valve) : valve(valve) {}
 
-	NoSuchValveError::NoSuchValveError(const NoSuchValveError& error) : BuildError(error), valve(valve) {}
+	NoSuchValveError::NoSuchValveError(const NoSuchValveError& error) : BuildError(error), valve(error.valve) {}
 
 	REDSTRAIN_DEFINE_ERROR(NoSuchValveError) {
 		out << "No such valve: " << valve;
