92cec3f13564afe6c7d2cedcd90eebb14068ac4d
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-07-30 00:20:20 +0200

    removed mod_build to start over :(

--- tools/build/src/build.cpp
+++ tools/build/src/build.cpp
@@ -1,55 +1,13 @@
-#include <redstrain/util/Delete.hpp>
-#include <redstrain/build/Project.hpp>
-#include <redstrain/platform/Console.hpp>
-#include <redstrain/platform/Pathname.hpp>
-#include <redstrain/build/BuildContext.hpp>
-#include <redstrain/io/FileOutputStream.hpp>
-#include <redstrain/build/ConsoleBuildUI.hpp>
-#include <redstrain/build/boot/XakeProject.hpp>
-#include <redstrain/build/NoSuchValveError.hpp>
-#include <redstrain/build/ComponentRuleBuilder.hpp>
-#include <redstrain/build/boot/XakeValveInjector.hpp>
-#include <redstrain/build/boot/XakeProjectFactory.hpp>
-#include <redstrain/build/boot/XakeComponentFinder.hpp>
-#include <redstrain/build/boot/XakeComponentFactory.hpp>
 #include <redstrain/error/UnsupportedOperationError.hpp>
-#include <redstrain/build/boot/XakeDependencyResolver.hpp>
-#include <redstrain/build/boot/XakeBuildArtifactMapper.hpp>
-#include <redstrain/build/boot/XakeBuildDirectoryMapper.hpp>
-#include <redstrain/build/DefaultComponentTypeStringifier.hpp>
 #include <redstrain/cmdline/parseopt.hpp>
-#include <redstrain/io/streamoperators.hpp>
 
 #include "Options.hpp"
 
 using std::string;
-using redengine::util::Delete;
-using redengine::build::Component;
-using redengine::platform::Console;
-using redengine::platform::Pathname;
-using redengine::build::StaticValve;
-using redengine::build::BuildContext;
 using redengine::cmdline::OptionLogic;
-using redengine::io::FileOutputStream;
-using redengine::build::ConsoleBuildUI;
-using redengine::build::ProjectBuilder;
-using redengine::build::NoSuchValveError;
 using redengine::cmdline::runWithOptions;
-using redengine::build::boot::XakeProject;
-using redengine::build::ComponentRuleBuilder;
-using redengine::build::boot::XakeValveInjector;
-using redengine::build::boot::XakeProjectFactory;
-using redengine::build::boot::XakeComponentFinder;
 using redengine::error::UnsupportedOperationError;
-using redengine::io::DefaultConfiguredOutputStream;
-using redengine::build::boot::XakeComponentFactory;
-using redengine::build::boot::XakeDependencyResolver;
-using redengine::build::boot::XakeBuildArtifactMapper;
-using redengine::build::boot::XakeBuildDirectoryMapper;
-using redengine::build::DefaultComponentTypeStringifier;
 using redengine::cmdline::ConfigurationObjectOptionLogic;
-using redengine::io::operator<<;
-using redengine::io::config::tabulation;
 
 int run(const string&, const Options&);
 int bootstrap(const string&, const Options&);
@@ -74,58 +32,8 @@ int run(const string& progname, const Options& options) {
 		return normal(progname, options);
 }
 
-int bootstrap(const string&, const Options& options) {
-	string base(Pathname::tidy(Pathname::join(Pathname::getWorkingDirectory(), options.getBase())));
-	XakeProject xproject(base);
-	XakeProjectFactory projectFactory(xproject);
-	XakeComponentFinder componentFinder(xproject);
-	XakeComponentFactory componentFactory(xproject);
-	XakeDependencyResolver dependencyResolver(xproject);
-	XakeBuildDirectoryMapper buildDirectoryMapper(xproject);
-	XakeBuildArtifactMapper buildArtifactMapper(xproject);
-	XakeValveInjector valveInjector(xproject);
-	ComponentRuleBuilder ruleBuilder(buildDirectoryMapper, buildArtifactMapper,
-			DefaultComponentTypeStringifier::instance, &valveInjector);
-	ProjectBuilder projectBuilder(projectFactory, componentFinder, componentFactory,
-			dependencyResolver, ruleBuilder);
-	projectBuilder.buildProject(base);
-	ConsoleBuildUI ui;
-	Delete<BuildContext> context(projectBuilder.newBuildContext(ui));
-	xproject.makeValveGroups(**context);
-	ui.setMinimalComponentTypeWidth(static_cast<unsigned>(
-			Component::getMaximalComponentTypeWidth(DefaultComponentTypeStringifier::instance)));
-	ui.setMinimalComponentNameWidth(static_cast<unsigned>(
-			projectBuilder.getProject()->getMaximalComponentNameWidth()));
-	Options::ValveNameIterator ovbegin, ovend;
-	options.getValves(ovbegin, ovend);
-	for(; ovbegin != ovend; ++ovbegin) {
-		StaticValve* valve = context->getValve(*ovbegin);
-		if(!valve)
-			throw NoSuchValveError(*ovbegin);
-		valve->setOpen(true);
-	}
-	if(options.isDumpContext()) {
-		FileOutputStream sout(Console::getStandardHandle(Console::STANDARD_OUTPUT));
-		DefaultConfiguredOutputStream<char>::Stream formatted(sout);
-		formatted << tabulation<char>("    ");
-		context->dumpContext(formatted);
-		return 0;
-	}
-	context->forceValveGroups();
-	if(options.isDry()) {
-		ui.startPredictiveRun();
-		bool didSomething = context->predictiveLoop();
-		ui.endPredictiveRun(didSomething);
-	}
-	else {
-		ui.setFlags(ConsoleBuildUI::PRINT_DEFINITIVE);
-		ui.startPredictiveRun();
-		bool didSomething = context->predictiveLoop();
-		ui.endPredictiveRun(didSomething);
-		ui.startDefinitiveRun();
-		didSomething = context->definitiveLoop();
-		ui.endDefinitiveRun(didSomething);
-	}
+int bootstrap(const string&, const Options&) {
+	//TODO
 	return 0;
 }
 
