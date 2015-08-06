c37128e3611fbb16f6d00fd510b91efc175b2739
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-08-06 12:48:23 +0200

    worked around GCC breakage in OfTwoIntegerTypes

--- modules/util/src/TypeCombinators.hpp
+++ modules/util/src/TypeCombinators.hpp
@@ -16,39 +16,41 @@ namespace util {
 			U_IS_WIDER
 		};
 
-		template<int, Width, bool, bool>
+		// We need to pass T and U through explicitly;
+		// otherwise GCC will derp with "internal compiler error"...
+		template<typename, typename, Width, bool, bool>
 		struct Impl;
 
 		#define REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(width, tSigned, uSigned, narrower, wider, lower, higher) \
-			template<int Dummy> \
-			struct Impl<Dummy, width, tSigned, uSigned> { \
+			template<typename T2, typename U2> \
+			struct Impl<T2, U2, width, tSigned, uSigned> { \
 				typedef narrower Narrower; \
 				typedef wider Wider; \
 				typedef lower WithLowerLowerBound; \
 				typedef higher WithHigherUpperBound; \
 			};
 
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, false, false, U, T, T, T)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, false, true,  U, T, U, T)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, true,  false, U, T, T, T)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, true,  true,  U, T, T, T)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, false, false, U2, T2, T2, T2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, false, true,  U2, T2, U2, T2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, true,  false, U2, T2, T2, T2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(T_IS_WIDER, true,  true,  U2, T2, T2, T2)
 
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, false, false, T, T, T, T)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, false, true,  T, T, U, T)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, true,  false, T, T, T, U)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, true,  true,  T, T, T, T)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, false, false, T2, T2, T2, T2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, false, true,  T2, T2, U2, T2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, true,  false, T2, T2, T2, U2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(SAME_WIDTH, true,  true,  T2, T2, T2, T2)
 
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, false, false, T, U, T, U)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, false, true,  T, U, U, U)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, true,  false, T, U, T, U)
-		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, true,  true,  T, U, U, U)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, false, false, T2, U2, T2, U2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, false, true,  T2, U2, U2, U2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, true,  false, T2, U2, T2, U2)
+		REDSTRAIN_UTIL_OFTWOINTEGERTYPES_IMPL(U_IS_WIDER, true,  true,  T2, U2, U2, U2)
 
 	  private:
 		static const Width WIDTH = sizeof(T) > sizeof(U) ? T_IS_WIDER
 				: (sizeof(U) > sizeof(T) ? U_IS_WIDER : SAME_WIDTH);
 
 	  private:
-		typedef Impl<0, WIDTH, IntegerBounds<T>::SIGNED, IntegerBounds<U>::SIGNED> I;
+		typedef Impl<T, U, WIDTH, IntegerBounds<T>::SIGNED, IntegerBounds<U>::SIGNED> I;
 
 	  public:
 		typedef typename I::Narrower Narrower;
