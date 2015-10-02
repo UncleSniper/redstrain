f879184cfa14f07029d95ebe3822b2a3c51f11a0
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-10-02 15:21:34 +0200

    added XakeComponentFactory

---
+++ modules/build/src/boot/XakeComponentFactory.cpp
@@ -0,0 +1,28 @@
+#include <redstrain/util/Delete.hpp>
+
+#include "XakeProject.hpp"
+#include "XakeComponent.hpp"
+#include "XakeComponentFactory.hpp"
+
+using std::string;
+using redengine::util::Delete;
+
+namespace redengine {
+namespace build {
+namespace boot {
+
+	XakeComponentFactory::XakeComponentFactory(XakeProject& project) : project(project) {}
+
+	XakeComponentFactory::XakeComponentFactory(const XakeComponentFactory& factory)
+			: ComponentFactory(factory), project(factory.project) {}
+
+	Component* XakeComponentFactory::newComponent(const Project&,
+			const string& baseDirectory, Component::Type type) {
+		Delete<XakeComponent> component(new XakeComponent(project, baseDirectory, type));
+		component->addSourceDirectory(project.getProjectConfiguration().getProperty(Resources::RES_SOURCE_DIRECTORY));
+		component->addLanguage(project.getCPPLanguage());
+		component->addLanguage(project.getObjectFileLanguage());
+		return component.set();
+	}
+
+}}}
---
+++ modules/build/src/boot/XakeComponentFactory.hpp
@@ -0,0 +1,35 @@
+#ifndef REDSTRAIN_MOD_BUILD_BOOT_XAKECOMPONENTFACTORY_HPP
+#define REDSTRAIN_MOD_BUILD_BOOT_XAKECOMPONENTFACTORY_HPP
+
+#include "../ComponentFactory.hpp"
+
+namespace redengine {
+namespace build {
+namespace boot {
+
+	class XakeProject;
+
+	class REDSTRAIN_BUILD_API XakeComponentFactory : public ComponentFactory {
+
+	  private:
+		XakeProject& project;
+
+	  public:
+		XakeComponentFactory(XakeProject&);
+		XakeComponentFactory(const XakeComponentFactory&);
+
+		inline XakeProject& getProject() {
+			return project;
+		}
+
+		inline const XakeProject& getProject() const {
+			return project;
+		}
+
+		virtual Component* newComponent(const Project&, const std::string&, Component::Type) = 0;
+
+	};
+
+}}}
+
+#endif /* REDSTRAIN_MOD_BUILD_BOOT_XAKECOMPONENTFACTORY_HPP */
