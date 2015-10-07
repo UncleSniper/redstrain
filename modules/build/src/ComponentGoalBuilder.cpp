5b6cb77fec30cd85e6e81a6472ee8c8b0f49dd7b
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-10-07 13:46:07 +0200

    added 'build:<flavor>' goals to ComponentGoalBuilder

--- modules/build/src/ComponentGoalBuilder.cpp
+++ modules/build/src/ComponentGoalBuilder.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <redstrain/util/Unref.hpp>
 
 #include "Project.hpp"
@@ -6,6 +7,7 @@
 #include "BuildContext.hpp"
 #include "ComponentGoalBuilder.hpp"
 
+using std::set;
 using std::string;
 using redengine::util::Unref;
 
@@ -37,6 +39,36 @@ namespace build {
 	void ComponentGoalBuilder::setupGoals(const Project& project, BuildContext& context) {
 		setupNamedGoal(project, context, "build");
 		setupNamedGoal(project, context, "clean");
+		set<Flavor> flavors;
+		Project::ComponentNameIterator cnbegin, cnend;
+		project.getComponents(cnbegin, cnend);
+		for(; cnbegin != cnend; ++cnbegin) {
+			Component* component = project.getComponent(*cnbegin);
+			if(!component)
+				continue;
+			Component::FlavoredArtifactIterator fabegin, faend;
+			component->getFinalArtifacts(fabegin, faend);
+			for(; fabegin != faend; ++fabegin) {
+				Flavor flavor(fabegin->getFlavor());
+				Goal* goal = context.getGoal(component->getGoalName() + ':' + flavor.getName());
+				if(goal)
+					flavors.insert(flavor);
+			}
+		}
+		set<Flavor>::const_iterator fbegin(flavors.begin()), fend(flavors.end());
+		for(; fbegin != fend; ++fbegin) {
+			Unref<MultiGoal> groupGoal(new MultiGoal);
+			project.getComponents(cnbegin, cnend);
+			for(; cnbegin != cnend; ++cnbegin) {
+				Component* component = project.getComponent(*cnbegin);
+				if(!component)
+					continue;
+				Goal* goal = context.getGoal(component->getGoalName() + ':' + fbegin->getName());
+				if(goal)
+					groupGoal->addGoal(*goal);
+			}
+			context.addGoal("build:" + fbegin->getName(), **groupGoal);
+		}
 		Goal* defaultGoal = context.getGoal("build");
 		if(defaultGoal)
 			context.setDefaultGoal(defaultGoal);
