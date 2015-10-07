b04ed9c00bba1737e957cc42f90a83665fd4f4d7
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-10-07 14:21:51 +0200

    added FileConversionLanguage

---
+++ modules/build/src/FileConversionLanguage.cpp
@@ -0,0 +1,70 @@
+#include <redstrain/util/Unref.hpp>
+#include <redstrain/platform/Pathname.hpp>
+
+#include "BuildContext.hpp"
+#include "FileConversionLanguage.hpp"
+
+using std::string;
+using redengine::util::Unref;
+using redengine::util::Appender;
+using redengine::platform::Pathname;
+
+namespace redengine {
+namespace build {
+
+	FileConversionLanguage::FileConversionLanguage(const string& name) : Language(name) {}
+
+	FileConversionLanguage::FileConversionLanguage(const FileConversionLanguage& language) : Language(language) {}
+
+	Flavor FileConversionLanguage::getTargetFlavor(const Flavor&, const Flavor& transformFlavor) {
+		return transformFlavor;
+	}
+
+	bool FileConversionLanguage::isTargetFinal(FileArtifact&, const Flavor&, const Flavor&,
+			FileArtifact&, const Flavor&, Component&) {
+		return false;
+	}
+
+	bool FileConversionLanguage::acceptsSource(const Flavor&, const Flavor&) {
+		return true;
+	}
+
+	static const Flavor *const FLAVORS[] = {
+		&Flavor::GENERIC,
+		NULL
+	};
+
+	void FileConversionLanguage::getSupportedFlavors(Component::Type, Appender<Flavor>& sink) {
+		Language::sinkSupportedFlavors(FLAVORS, sink);
+	}
+
+	bool FileConversionLanguage::isOneToOne(const Flavor&) {
+		return true;
+	}
+
+	FileArtifact* FileConversionLanguage::getSourceTransform(BuildContext& context, const string& sourceDirectory,
+			FileArtifact& sourceArtifact, const Flavor& sourceFlavor, const string& targetDirectory,
+			const Flavor& transformFlavor, Component& component, BuildArtifactMapper&,
+			ManyToOneTransform<FileArtifact>*& manyTransform, Flavor& targetFlavor, bool& isFinal) {
+		string sourceTail(Pathname::stripPrefix(sourceArtifact.getPath(), sourceDirectory));
+		string targetBasename(getTargetBasename(Pathname::basename(sourceTail),
+				sourceFlavor, transformFlavor, component));
+		string targetTail(Pathname::join(Pathname::dirname(sourceTail, Pathname::LOGICAL), targetBasename));
+		FileArtifact& target = context.internFileArtifact(Pathname::join(targetDirectory, targetTail), targetTail);
+		Unref<Transform> transform(getConversionTransform(sourceArtifact, sourceFlavor, transformFlavor, component));
+		transform->addPrerequisite(sourceArtifact);
+		target.setGeneratingTransform(*transform);
+		target.addIntermediateDirectories(context, component.getBaseDirectory());
+		manyTransform = NULL;
+		targetFlavor = getTargetFlavor(sourceFlavor, transformFlavor);
+		isFinal = isTargetFinal(sourceArtifact, sourceFlavor, transformFlavor, target, targetFlavor, component);
+		target.ref();
+		return &target;
+	}
+
+	FileArtifact* FileConversionLanguage::getHeaderExposeTransform(BuildContext&, const string&, FileArtifact&,
+			const Flavor&, const string&, Component&, BuildArtifactMapper&, Flavor&) {
+		return NULL;
+	}
+
+}}
---
+++ modules/build/src/FileConversionLanguage.hpp
@@ -0,0 +1,35 @@
+#ifndef REDSTRAIN_MOD_BUILD_CODETABLEDEFINITIONLANGUAGE_HPP
+#define REDSTRAIN_MOD_BUILD_CODETABLEDEFINITIONLANGUAGE_HPP
+
+#include "Language.hpp"
+
+namespace redengine {
+namespace build {
+
+	class REDSTRAIN_BUILD_API FileConversionLanguage : public Language {
+
+	  protected:
+		virtual std::string getTargetBasename(const std::string&, const Flavor&, const Flavor&, Component&) = 0;
+		virtual Transform* getConversionTransform(FileArtifact&, const Flavor&, const Flavor&, Component&) = 0;
+		virtual Flavor getTargetFlavor(const Flavor&, const Flavor&);
+		virtual bool isTargetFinal(FileArtifact&, const Flavor&, const Flavor&,
+				FileArtifact&, const Flavor&, Component&);
+
+	  public:
+		FileConversionLanguage(const std::string&);
+		FileConversionLanguage(const FileConversionLanguage&);
+
+		virtual bool acceptsSource(const Flavor&, const Flavor&);
+		virtual void getSupportedFlavors(Component::Type, util::Appender<Flavor>&);
+		virtual bool isOneToOne(const Flavor&);
+		virtual FileArtifact* getSourceTransform(BuildContext&, const std::string&, FileArtifact&,
+				const Flavor&, const std::string&, const Flavor&, Component&, BuildArtifactMapper&,
+				ManyToOneTransform<FileArtifact>*&, Flavor&, bool&) = 0;
+		virtual FileArtifact* getHeaderExposeTransform(BuildContext&, const std::string&, FileArtifact&,
+				const Flavor&, const std::string&, Component&, BuildArtifactMapper&, Flavor&);
+
+	};
+
+}}
+
+#endif /* REDSTRAIN_MOD_BUILD_CODETABLEDEFINITIONLANGUAGE_HPP */
