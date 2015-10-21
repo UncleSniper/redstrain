22950a342c4caab0cd4d4b92cb4fba110e3d07ed
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-10-21 14:44:04 +0200

    added PolicyLocker

---
+++ modules/platform/src/PolicyLocker.hpp
@@ -0,0 +1,60 @@
+#ifndef REDSTRAIN_MOD_PLATFORM_POLICYLOCKER_HPP
+#define REDSTRAIN_MOD_PLATFORM_POLICYLOCKER_HPP
+
+#include <list>
+
+namespace redengine {
+namespace platform {
+
+	template<typename SubjectT>
+	class PolicyLocker {
+
+	  private:
+		const SubjectT* object;
+
+	  public:
+		PolicyLocker(SubjectT* object = NULL) : object(object) {
+			if(object)
+				object->lockByPolicy();
+		}
+
+		PolicyLocker(const PolicyLocker& locker) : object(locker.object) {
+			if(object)
+				object->lockByPolicy();
+		}
+
+		~PolicyLocker() {
+			if(object)
+				object->uncheckedUnlockByPolicy();
+		}
+
+		void release() {
+			if(object) {
+				object->unlockByPolicy();
+				object = NULL;
+			}
+		}
+
+		PolicyLocker& operator=(SubjectT* newObject) {
+			if(newObject)
+				newObject->lockByPolicy();
+			if(object)
+				object->unlockByPolicy();
+			object = newObject;
+			return *this;
+		}
+
+		PolicyLocker& operator=(const PolicyLocker& locker) {
+			if(locker.object)
+				locker.object->lockByPolicy();
+			if(object)
+				object->unlockByPolicy();
+			object = locker.object;
+			return *this;
+		}
+
+	};
+
+}}
+
+#endif /* REDSTRAIN_MOD_PLATFORM_POLICYLOCKER_HPP */
