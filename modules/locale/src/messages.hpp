83f92ef57c6078e0be9530089f40f0668885ee14
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-10-22 15:35:04 +0200

    added compileMessages() to mod_locale

---
+++ modules/locale/src/messages.hpp
@@ -0,0 +1,121 @@
+#ifndef REDSTRAIN_MOD_LOCALE_MESSAGES_HPP
+#define REDSTRAIN_MOD_LOCALE_MESSAGES_HPP
+
+#include <map>
+#include <set>
+#include <list>
+#include <redstrain/text/StringUtils.hpp>
+#include <redstrain/text/TranscodeForError.hpp>
+#include <redstrain/io/FormattedInputStream.hpp>
+#include <redstrain/io/MissingInputSeparatorError.hpp>
+
+#include "MessageCache.hpp"
+#include "MissingMessageKeyError.hpp"
+#include "DuplicateMessageKeyError.hpp"
+#include "UndefinedMessageKeyError.hpp"
+
+namespace redengine {
+namespace locale {
+
+	template<typename CharT>
+	void getMessageKeyOrder(const std::string& inputStreamName, io::InputStream<CharT>& stream,
+			std::map<std::basic_string<CharT>, size_t>* mapOrder, std::list<std::basic_string<CharT> >* listOrder) {
+		typedef std::basic_string<CharT> String;
+		typedef typename String::size_type StringLength;
+		if(mapOrder)
+			mapOrder->clear();
+		if(listOrder)
+			listOrder->clear();
+		io::FormattedInputStream<CharT> formattedInput(stream);
+		String line;
+		unsigned lno = 0u;
+		std::set<std::basic_string<CharT> > knownKeys;
+		while(formattedInput.readLine(line)) {
+			++lno;
+			line = text::StringUtils<CharT>::trim(line, text::StringUtils<CharT>::TRIM_FRONT);
+			if(line.empty())
+				continue;
+			if(line[0] == static_cast<CharT>('#')) {
+				line.clear();
+				continue;
+			}
+			StringLength pos = line.find(static_cast<CharT>('#'));
+			if(pos == String::npos)
+				throw io::MissingInputSeparatorError("=", inputStreamName, lno);
+			String key(line.substr(static_cast<StringLength>(0u), pos));
+			if(mapOrder) {
+				if(mapOrder->find(key) != mapOrder->end())
+					throw DuplicateMessageKeyError(text::TranscodeForError<CharT>::toCharString(key),
+							inputStreamName, lno);
+				size_t index = static_cast<size_t>(mapOrder->size());
+				(*mapOrder)[key] = index;
+			}
+			else {
+				if(knownKeys.find(key) != knownKeys.end())
+					throw DuplicateMessageKeyError(text::TranscodeForError<CharT>::toCharString(key),
+							inputStreamName, lno);
+				knownKeys.insert(key);
+			}
+			if(listOrder)
+				listOrder->push_back(key);
+			line.clear();
+		}
+	}
+
+	template<typename CharT>
+	void compileMessages(const std::string& inputStreamName, io::InputStream<CharT>& inputStream,
+			io::OutputStream<CharT>& outputStream, const std::list<std::basic_string<CharT> >* order) {
+		typedef std::basic_string<CharT> String;
+		typedef typename String::size_type StringLength;
+		typedef std::pair<String, unsigned> PrecachePair;
+		typedef std::map<String, PrecachePair> Precache;
+		io::FormattedInputStream<CharT> formattedInput(inputStream);
+		String line;
+		unsigned lno = 0u;
+		Precache precache;
+		MessageCache<CharT> cache;
+		while(formattedInput.readLine(line)) {
+			++lno;
+			line = text::StringUtils<CharT>::trim(line, text::StringUtils<CharT>::TRIM_FRONT);
+			if(line.empty())
+				continue;
+			if(line[0] == static_cast<CharT>('#')) {
+				line.clear();
+				continue;
+			}
+			StringLength pos = line.find(static_cast<CharT>('#'));
+			if(pos == String::npos)
+				throw io::MissingInputSeparatorError("=", inputStreamName, lno);
+			String key(line.substr(static_cast<StringLength>(0u), pos));
+			String value(line.substr(pos + static_cast<StringLength>(1u)));
+			if(precache.find(key) != precache.end())
+				throw DuplicateMessageKeyError(text::TranscodeForError<CharT>::toCharString(key),
+						inputStreamName, lno);
+			precache[key] = PrecachePair(value, lno);
+			if(!order)
+				cache.addMessage(key, value);
+			line.clear();
+		}
+		if(order) {
+			typename std::list<std::basic_string<CharT> >::const_iterator
+					obegin(order->begin()), oend(order->end());
+			for(; obegin != oend; ++obegin) {
+				typename Precache::iterator it(precache.find(*obegin));
+				if(it == precache.end())
+					throw MissingMessageKeyError(text::TranscodeForError<CharT>::toCharString(*obegin),
+							inputStreamName);
+				cache.addMessage(it->first, it->second.first);
+				precache.erase(it);
+			}
+			if(!precache.empty()) {
+				typename Precache::iterator front = precache.begin();
+				throw UndefinedMessageKeyError(text::TranscodeForError<CharT>::toCharString(front->first),
+						inputStreamName, front->second.second);
+			}
+		}
+		cache.saveTo(outputStream);
+	}
+
+}}
+
+#endif /* REDSTRAIN_MOD_LOCALE_MESSAGES_HPP */
