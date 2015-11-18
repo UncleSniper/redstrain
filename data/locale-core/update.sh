a920e406c5358d84c5a74fd6fa27adbb33495b78
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2015-11-18 15:19:54 +0100

    fixed data/locale-core/update.sh to cd to correct dir

--- data/locale-core/update.sh
+++ data/locale-core/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd "$(dirname -- "$0")" || exit 1
+
 for def in src/*.msgdf; do
 	echo "=== Updating: $def"
 	../../tools/updmsgs/redupdmsgs-static -v src/messages.msgsc "$def" || exit 1
