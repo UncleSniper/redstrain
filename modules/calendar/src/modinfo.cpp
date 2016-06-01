02cd4a37557985bdb4d1df4ae253bc9db1791162
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2016-06-01 13:03:53 +0200

    updated modinfo of mod_calendar

--- modules/calendar/src/modinfo.cpp
+++ modules/calendar/src/modinfo.cpp
@@ -1,7 +1,11 @@
+#include <redstrain/io/modinfo.hpp>
 #include <redstrain/util/modinfo.hpp>
+#include <redstrain/text/modinfo.hpp>
 #include <redstrain/error/modinfo.hpp>
+#include <redstrain/locale/modinfo.hpp>
 #include <redstrain/redmond/modinfo.hpp>
 #include <redstrain/platform/modinfo.hpp>
+#include <redstrain/protostr/modinfo.hpp>
 
 #include "modinfo.hpp"
 
@@ -18,5 +22,13 @@ namespace calendar {
 			RedStrain/Calendar, RedStrain/Util, REDSTRAIN_MOD_UTIL, ::redengine::util)
 	REDSTRAIN_DEPEND_MODULE(Platform,
 			RedStrain/Calendar, RedStrain/Platform, REDSTRAIN_MOD_PLATFORM, ::redengine::platform)
+	REDSTRAIN_DEPEND_MODULE(IO,
+			RedStrain/Calendar, RedStrain/IO, REDSTRAIN_MOD_IO, ::redengine::io)
+	REDSTRAIN_DEPEND_MODULE(ProtoStreams,
+			RedStrain/Calendar, RedStrain/ProtoStreams, REDSTRAIN_MOD_PROTOSTR, ::redengine::protostr)
+	REDSTRAIN_DEPEND_MODULE(Text,
+			RedStrain/Calendar, RedStrain/Text, REDSTRAIN_MOD_TEXT, ::redengine::text)
+	REDSTRAIN_DEPEND_MODULE(Locale,
+			RedStrain/Calendar, RedStrain/Locale, REDSTRAIN_MOD_LOCALE, ::redengine::locale)
 
 }}
